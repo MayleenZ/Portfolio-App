7f03bd1ad9f00da82762972201462148ed457c71
Mayleen <mayleennyc@gmail.com>
2023-03-28 15:41:26 -0400

    day1new

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,12 +1,12 @@
-import DictionaryAPI from "../services/DictionaryAPI";
+// import DictionaryAPI from "../services/DictionaryAPI";
 
-function Projects() {
-  return (
-    <div>
-      <h1>Project</h1>
-      {/* <DictionaryAPI /> */}
-    </div>
-  );
-}
+// function Projects() {
+//   return (
+//     <div>
+//       <h1>Project</h1>
+//       {/* <DictionaryAPI /> */}
+//     </div>
+//   );
+// }
 
-export default Projects;
+// export default Projects;

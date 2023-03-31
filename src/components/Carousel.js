7666c9cd36c4ec180ff172d528bbd8092b60824c
Mayleen <mayleennyc@gmail.com>
2023-03-31 16:58:49 -0400

    day4

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,43 +4,80 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const CarouselPage = () => {
   return (
-    <div>
+    <div className="Carousel">
       <Carousel>
         <div>
-            <h1>Nike Sneaker Site</h1>
-          <a href="https://nikesneakersite.netlify.app/">Nike Sneaker Site</a>
+          <a href="https://nikesneakersite.netlify.app/" className="link-site"             target="_blank"
+            rel="noopener">
+            Nike Sneaker Site
+          </a>
           <img
             src="https://i.postimg.cc/PrQWdP6Z/Screen-Shot-2023-03-31-at-1-26-46-PM.png"
-            width="200"
-            height="auto"
+            alt="sneaker website"
           />
-          <p className="legend">Home Page</p>
+          <p className="legend">Nike Home Page</p>
         </div>
         <div>
-          <img src="https://i.postimg.cc/9QNqfTy4/Screen-Shot-2023-03-31-at-1-27-21-PM.png" />
-          <p className="legend">Checkout Page</p>
+          <img
+            src="https://i.postimg.cc/9QNqfTy4/Screen-Shot-2023-03-31-at-1-27-21-PM.png"
+            target="_blank"
+            rel="noopener"
+            alt="homepage"
+          />
+          <p className="legend">Nike Checkout Page</p>
         </div>
         <div>
-          <h1>Star War Spaceships</h1>
-          <a href="https://starwarstarshipcards.netlify.app">
+          <a
+            href="https://starwarstarshipcards.netlify.app"
+            className="link-site"
+            target="_blank"
+            rel="noopener"
+          >
             Star War Spaceships
           </a>
-          <img src="https://i.postimg.cc/52vqDnh4/Screen-Shot-2023-03-31-at-1-43-43-PM.png" />
-          <p className="legend">Legend 2</p>
+          <img
+            src="https://i.postimg.cc/52vqDnh4/Screen-Shot-2023-03-31-at-1-43-43-PM.png"
+            alt="spaceship website"
+          />
+          <p className="legend">Space Ship Cards</p>
         </div>
         <div>
-            <h1>BuzzWord</h1>
-          <a href="https://buzzwordgame.netlify.app">BuzzWord</a>
-          <img src="https://i.postimg.cc/9QNqfTy4/Screen-Shot-2023-03-31-at-1-27-21-PM.png" />
-          <p className="legend">Legend 2</p>
+          <a
+            href="https://buzzwordgame.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+            alt="word-game"
+            className="link-site"
+          >
+            BuzzWord
+          </a>
+          <img
+            src="https://i.postimg.cc/QCLrJcyn/Screen-Shot-2023-03-31-at-2-04-12-PM.png"
+            alt="word-game website"
+          />
+          <p className="legend">BuzzWord Home Page</p>
         </div>
         <div>
-          <h1>Cat Facts & NYC Adoption </h1>
-          <a href="https://catfactsadopt.netlify.app">
+          <img
+            src="https://i.postimg.cc/qM0PBBVn/Screen-Shot-2023-03-31-at-2-08-01-PM.png"
+            alt="word-game website"
+          />
+          <p className="legend">Winner Outcome</p>
+        </div>
+        <div>
+          <a
+            href="https://catfactsadopt.netlify.app"
+            className="link-site"
+            target="_blank"
+            rel="noopener"
+          >
             Cat Facts & NYC Adoption
           </a>
-          <img src="https://i.postimg.cc/9QNqfTy4/Screen-Shot-2023-03-31-at-1-27-21-PM.png" />
-          <p className="legend">Legend 2</p>
+          <img
+            src="https://i.postimg.cc/zGY1gwgL/Screen-Shot-2023-03-31-at-3-14-46-PM.png"
+            alt="cat facts website"
+          />
+          <p className="legend">Cat Fact Page</p>
         </div>
       </Carousel>
     </div>
